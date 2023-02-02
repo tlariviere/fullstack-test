bf6fbade59552be76166caa85263d530e2721a3b
Thibaud LARIVIERE <thibaud@agro-league.com>
2023-02-02 09:43:38 +0100

    Format with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className='text-lg font-bold'>Genesis Indicators</h1>
+      <h1 className="text-lg font-bold">Genesis Indicators</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
